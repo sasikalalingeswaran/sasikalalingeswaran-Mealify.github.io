f8a24d65f7f5594eb75b28ad1568b7b74d0d716b
sasikalalingeswaran <144766473+sasikalalingeswaran@users.noreply.github.com>
2026-06-24 16:55:57 +0530

    Add files via upload

--- src/api/mealdb.js
+++ src/api/mealdb.js
@@ -1,1 +1,8 @@
+import axios from "axios"
 
+const BASE='https://www.themealdb.com/api/json/v1/1'
+
+export const fetchCatergory=()=>axios.get(`${BASE}/categories.php`)
+export const fetchByCatergories=(cat)=>axios.get(`${BASE}/filter.php?c=${cat}`)
+export const fetchById=(id)=>axios.get(`${BASE}/lookup.php?i=${id}`)
+export const searchByCatergory=(query)=>axios.get(`${BASE}/search.php?s=${query}`)
